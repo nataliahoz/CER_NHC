43d1276e7a767e54847053044e8b631ee1716045
nataliahoz <33428543+nataliahoz@users.noreply.github.com>
2017-11-08 10:52:05 +0100

    Crear tabla de datos

--- templates/datos-form.php
+++ templates/datos-form.php
@@ -33,6 +33,87 @@
     <meta charset="utf-8">
     <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
+    <script Language="JavaScript">
+      function genera_tabla() {
+      // Obtener la referencia del elemento body
+      var body = document.getElementsByTagName("body")[0];
+      // Crea un elemento <table> y un elemento <tbody>
+      var tabla   = document.createElement("table");
+      var tblBody = document.createElement("tbody");
+      // Crea las celdas
+      for (var i = 0; i < 2; i++) {
+       // Crea las hileras de la tabla
+       var hilera = document.createElement("tr");
+       for (var j = 0; j < 2; j++) {
+         // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+         // texto sea el contenido de <td>, ubica el elemento <td> al final
+         // de la hilera de la tabla
+         var celda = document.createElement("td");
+         var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
+         celda.appendChild(textoCelda);
+         hilera.appendChild(celda);
+       }
+       // agrega la hilera al final de la tabla (al final del elemento tblbody)
+       tblBody.appendChild(hilera);
+      }
+      // posiciona el <tbody> debajo del elemento <table>
+      tabla.appendChild(tblBody);
+      // appends <table> into <body>
+      body.appendChild(tabla);
+      // modifica el atributo "border" de la tabla y lo fija a "2";
+      tabla.setAttribute("border", "2");
+      }
+   </script>
+   <script Language="JavaScript">
+   
+      <!-- Helpers for JSI page...-->
+      // Navigation - Start
+      function goback(){
+      alert("Good Bye!");
+      history.go(-1);
+      }
+      function gettheDate() {
+      Todays = new Date();
+      TheDate = "" + (Todays.getMonth()+ 1) +" / "+ Todays.getDate() + " / " + (Todays.getYear() + 1900) 
+      document.clock.thedate.value = TheDate;
+      }
+      // Navigation - Stop
+      // Netscapes Clock - Start
+      // this code was taken from Netscapes JavaScript documentation at
+      // www.netscape.com on Jan.25.96
+      var timerID = null;
+      var timerRunning = false;
+      function stopclock (){
+      if(timerRunning)
+      clearTimeout(timerID);
+      timerRunning = false;
+      }
+      function startclock () {
+      // Make sure the clock is stopped
+      stopclock();
+      gettheDate()
+      showtime();
+      }
+      function showtime () {
+      var now = new Date();
+      var hours = now.getHours();
+      var minutes = now.getMinutes();
+      var seconds = now.getSeconds()
+      var timeValue = "" + ((hours >12) ? hours -12 :hours)
+      timeValue += ((minutes < 10) ? ":0" : ":") + minutes
+      timeValue += ((seconds < 10) ? ":0" : ":") + seconds
+      timeValue += (hours >= 12) ? " P.M." : " A.M."
+      document.clock.face.value = timeValue;
+      // you could replace the above with this
+      // and have a clock on the status bar:
+      // window.status = timeValue;
+      timerID = setTimeout("showtime()",1000);
+      timerRunning = true;
+      }
+      // Netscapes Clock - Stop
+      // end Helpers -->
+   
+   </script>
 
 </head>
  
@@ -79,6 +160,15 @@
             </table>-->
             <!--<li >{{listinf}}</li>-->
             {{lbbtinf}}
+        <table border>
+         <tr>
+            <td><form name="clock" onSubmit="0"></td>
+         </tr>
+         <tr>
+            <td colspan=2>Hoy es: <input type="text" name="thedate" size=12 value=""></td>
+            <td colspan=2>La hora es: <input type="text" name="face" size=12 value=""></td></form>
+         </tr>
+         </table>
         <footer class="footer">
             <p>&copy; Natalia de la Hoz Carracedo - Computacion en red - UAH 2017</p>
         </footer>
