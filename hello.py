13e659e093560c879b2d4e079ca3d12380dfd8ca
nataliahoz <33428543+nataliahoz@users.noreply.github.com>
2017-12-14 19:37:22 +0100

    Final

--- hello.py
+++ hello.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python
 #"*** coding:"utf*8"**
 
-from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, render_template, flash, request, jsonify
 from pymongo import MongoClient
 from beebotte import *
 
 import requests
 import re
 
-#app = Flask(__name__)
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def main():
    global flag
+   global medcal
+   global actcal
    flag=0
+   medcal=0
+   actcal=0
    return render_template('index.html')
 
-@app.route('/loc')
-def location(): 
-   return '<p> UAH.EPS.EL10 </p>'
-
-@app.route('/datos-form.php', methods=['GET','POST'])
+@app.route('/datos-form.html', methods=['GET','POST'])
 def result():      
    #Leer base de datos y ver que valores estan fuera del intervalo umbral
    umbsup=request.form['umbsup']
@@ -32,38 +30,37 @@ def result():
    listinf=[]
    [listsup,listinf,lbbtsup,lbbtinf]=busq_resultado(umbsup,umbinf)
    #print(listsup,listinf)
-   #Ver cual es el ultimo dato que supero el umbral --> Destacar en negrita
    return render_template('datos-form.php', umbsup=umbsup , umbinf=umbinf, listsup=listsup, listinf=listinf, lbbtsup=lbbtsup, lbbtinf=lbbtinf)
-   #return send_from_directory(app.static_folder, request.path[1:],umbsup=umbsup , umbinf=umbinf, listsup=listsup, listinf=listinf, lbbtsup=lbbtsup, lbbtinf=lbbtinf)
 
 def busq_resultado(umbsup,umbinf):
-   lsup=[]
-   linf=[]
-   lbsup=[]
-   lbinf=[]
-   pos=0
-   pos2=0
-   strn=' '
-   strn2=' '
+   lsup = []
+   linf = []
+   lbsup = []
+   lbinf = []
+   pos = 0
+   pos2 = 0
+   strn = ' '
+   strn2 = ' '
    
    ##MongoDB
    mongoClient = MongoClient('localhost',27017) #Conexion con MongoDB (port 27017)
    db = mongoClient.Aleatorios        #Definicion de la base de datos
    coll_num = db.Numeros              #Definicion de la coleccion de numeros
    coll_dt = db.Date                  #Definicion de la coleccion de fechas
-   coll_tm =db.Time                   #Definicion de la coleccion de horas
-   cursor_num=coll_num.find()
-   cursor_dt=coll_dt.find()
-   cursor_tm=coll_tm.find() 
+   coll_tm = db.Time                   #Definicion de la coleccion de horas
+   cursor_num = coll_num.find()
+   cursor_dt = coll_dt.find()
+   cursor_tm = coll_tm.find() 
    ##Beebotte
    bbtClient = BBT('7e43ccf2633ab5fb261d9fcc18d65e13', 'c79e5a07f84c3e605b9f6ca11d8218f89cea7cd157637450b3f6f0bebc9e2924')    #Conexion con Beebotte
    res1 = Resource(bbtClient, 'dev', 'res1')   #Resource Numbers
    res2 = Resource(bbtClient, 'dev', 'res2')   #Resource Fechas
    res3 = Resource(bbtClient, 'dev', 'res3')   #Resource Horas
-   record_num=res1.read()
-   record_dt=res2.read()
-   record_tm=res3.read()
+   record_num = res1.read()
+   record_dt = res2.read()
+   record_tm = res3.read()
    
+   print('1-'+str(pos))
    for y in cursor_num:
       strn=str(y['valor'])+'****'+str(cursor_dt[pos]['fecha'])+'****'+str(cursor_tm[pos]['hora'])
       if (float(y['valor']) > float(umbsup)):
@@ -73,7 +70,8 @@ def busq_resultado(umbsup,umbinf):
       else:
          continue
       pos=pos+1
-      
+   
+   print('2-'+str(pos2))
    for z in record_num:
       strn2=str(z['data'])+'****'+str(record_dt[pos2]['data'])+'****'+str(record_tm[pos2]['data'])
       if (float(z['data']) > float(umbsup)):
@@ -89,77 +87,49 @@ def busq_resultado(umbsup,umbinf):
    
    return lsup,linf,lbsup,lbinf
 
-@app.route('/datos-media', methods=['GET'])
+@app.route('/index.html', methods=['POST'])
 def media():
-   bbdd=' '
-   suma=0
-   cont=0
-   media=0
+   tipomedia=request.form['tipomedia']
+   bbdd = ' '
+   suma = 0 
+   cont = 0
+   media = 0
+   global medcal
+   global actcal
    global flag
-   if flag % 2 == 0:
-      bbdd='MongoDB'
+   if ((flag % 2 == 0 and tipomedia == '1') or tipomedia == '2'):
+      bbdd = 'MongoDB'
       ##MongoDB
       mongoClient = MongoClient('localhost',27017) #Conexion con MongoDB (port 27017)
       db = mongoClient.Aleatorios        #Definicion de la base de datos
       coll_num = db.Numeros              #Definicion de la coleccion de numeros
-      cursor_num=coll_num.find()
+      cursor_num = coll_num.find()
       for y in cursor_num:
-         suma=float(suma)+float(y['valor'])
-         cont=cont+1
-      media=float(suma)/cont
-      flag=1
-   else:
-      bbdd='Beebotte'
+         suma = float(suma)+float(y['valor'])
+         cont = cont+1
+      media = float(suma)/cont
+      flag = 1
+   elif ((flag % 2 != 0 and tipomedia == '1') or tipomedia == '3'):
+      bbdd = 'Beebotte'
       ##Beebotte
       bbtClient = BBT('7e43ccf2633ab5fb261d9fcc18d65e13', 'c79e5a07f84c3e605b9f6ca11d8218f89cea7cd157637450b3f6f0bebc9e2924')    #Conexion con Beebotte
       res1 = Resource(bbtClient, 'dev', 'res1')   #Resource Numbers
-      record_num=res1.read()
+      record_num = res1.read()
       for z in record_num:
-         suma=float(suma)+float(z['data'])
-         cont=cont+1
-      media=float(suma)/cont
-      flag=0
-   print (media,bbdd)
-   return render_template('datos-media', media=media, bbdd=bbdd)
+         suma = float(suma)+float(z['data'])
+         cont = cont+1
+      media = float(suma)/cont
+      flag = 0
+   medcal = 1   
+   return render_template('index.html', media=media, bbdd=bbdd, medcal=medcal)
 
-@app.route('/datos-captura', methods=['POST'])
-def captura():
-   umbcap=request.form['umbcap']
-   rescap=0
-   url = 'http://www.numeroalazar.com.ar/'
-   regexp=r'([0-9]+.[0-9]+)'
-   response = requests.get(url)
-   #print(response)
-   if response.status_code != 200:
-      print('Failed to get data:',response.status_code)
-   else:
-      str1="numeros_generados"
-      str2="</div>"
-      shtml=response.text
-      pinum=shtml.find(str1)
-      pfnum=shtml[pinum:].find(str2)
-      number = re.findall(regexp, shtml[pinum:pinum+pfnum])
-   for x in number:
-      print (float(x))
-      if (float(x) > float(umbcap)):
-         rescap=float(x)
-         timestamp= str(time.strftime("%d/%m/%y"))+' '+str(time.strftime("%H:%M:%S"))
-         break
-         
-   return render_template('datos-captura', umbcap=umbcap, rescap=rescap, timestamp=timestamp)
-
-@app.route('/datos-grafica', methods=['GET'])
+@app.route('/datos-grafica', methods=['POST'])
 def grafica():
-   ##MongoDB
-   mongoClient = MongoClient('localhost',27017) #Conexion con MongoDB (port 27017)
-   db = mongoClient.Aleatorios        #Definicion de la base de datos
-   coll_num = db.Numeros              #Definicion de la coleccion de numeros
-   
-   ##Beebotte
-   bbtClient = BBT('7e43ccf2633ab5fb261d9fcc18d65e13', 'c79e5a07f84c3e605b9f6ca11d8218f89cea7cd157637450b3f6f0bebc9e2924')    #Conexion con Beebotte
-   res1 = Resource(bbtClient, 'dev', 'res1')   #Resource Numbers
-   
    return render_template('datos-grafica')
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0')
+   try:
+      app.run(host='0.0.0.0')
+   except KeyboardInterrupt:
+        print 'Interrupted'
+        sys.exit(0)
--- templates/datos-form.php
+++ templates/datos-form.php
@@ -22,7 +22,7 @@
      width: 200px;
    }
    footer {
-      position: absolute;
+      position: relative;
       bottom: 0;
       width: 100%;
    }
@@ -32,24 +32,40 @@
     <title>Random Numbers App</title>
     <meta charset="utf-8">
     <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
     <script Language="JavaScript">
-      function genera_tabla(arg) {
+      function genera_tabla(arg1,arg2,arg3,arg4) {
       // Obtener la referencia del elemento body
       var body = document.getElementsByTagName("body")[0];
       // Crea un elemento <table> y un elemento <tbody>
       var tabla   = document.createElement("table");
       var tblBody = document.createElement("tbody");
       // Crea las celdas
-      for (var i = 0; arg[i]!=0; i++) {
+      textoini=["Superiores en MongoDB","Inferiores en MongoDB","Superiores en Beebotte","Inferiores en Beebotte"]
+      maxlen=Math.max(arg1.length,arg2.length,arg3.length,arg4.length)
+      for (var i = 0; i<maxlen+1 ; i++) {
        // Crea las hileras de la tabla
        var hilera = document.createElement("tr");
-       for (var j = 0; j < 1; j++) {
+       for (var j = 0; j < 4; j++) {
+         if (j==0)
+            arg=arg1
+         else if (j==1)
+            arg=arg2
+         else if (j==2)
+            arg=arg3
+         else if (j==3)
+            arg=arg4
+         
          // Crea un elemento <td> y un nodo de texto, haz que el nodo de
          // texto sea el contenido de <td>, ubica el elemento <td> al final
          // de la hilera de la tabla
          var celda = document.createElement("td");
-         var textoCelda = document.createTextNode(arg[i]);
+         if (i==0) 
+            var textoCelda = document.createTextNode(textoini[j]);
+         else if (arg[i-1] != null)
+            var textoCelda = document.createTextNode(arg[i-1]);
+         else if (arg[i-1] == null)
+            var textoCelda = document.createTextNode(" ");
+         
          celda.appendChild(textoCelda);
          hilera.appendChild(celda);
        }
@@ -62,113 +78,143 @@
       body.appendChild(tabla);
       // modifica el atributo "border" de la tabla y lo fija a "2";
       tabla.setAttribute("border", "2");
+      tabla.setAttribute("align", "center");
+      alert("Resultados estan cargados");
+      }
+            // This is called with the results from from FB.getLoginStatus().
+      function statusChangeCallback(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+        // The response object is returned with a status field that lets the
+        // app know the current login status of the person.
+        // Full docs on the response object can be found in the documentation
+        // for FB.getLoginStatus().
+        if (response.status === 'connected') {
+          // Logged into your app and Facebook.
+          console.log('Ya conectado');     
+          document.getElementById('btn-log-out').style.display = 'block';
+          testAPI();
+        } else {
+         console.log('A conectarse!!!');
+          // The person is not logged into your app or we are unable to tell.
+          document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+          document.getElementById('btn-log-out').style.display = 'none';
+          
+        }
+      }
+    
+      // This function is called when someone finishes with the Login
+      // Button.  See the onlogin handler attached to it in the sample
+      // code below.
+      function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+        });
+      }
+    
+      window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '886609554837626',
+        cookie     : true,  // enable cookies to allow the server to access 
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.8' // use graph api version 2.8
+      });
+    
+      // Now that we've initialized the JavaScript SDK, we call 
+      // FB.getLoginStatus().  This function gets the state of the
+      // person visiting this page and can return one of three states to
+      // the callback you provide.  They can be:
+      //
+      // 1. Logged into your app ('connected')
+      // 2. Logged into Facebook, but not your app ('not_authorized')
+      // 3. Not logged into Facebook and can't tell if they are logged into
+      //    your app or not.
+      //
+      // These three cases are handled in the callback function.
+    
+      FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+      });
+      FB.AppEvents.logPageView(); 
+    
+      };
+    
+      // Load the SDK asynchronously
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    
+      // Here we run a very simple test of the Graph API after login is
+      // successful.  See statusChangeCallback() for when this call is made.
+      function testAPI() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Successful login for: ' + response.name);
+          document.getElementById('status').innerHTML =
+            'Aqui estan los datos solicitados, ' + response.name + '!';
+        });
+      
+      }
+      function mysignout(){
+         FB.logout(function(response)
+         {
+             console.log('Successful logout for: ' + response.name); 
+             document.getElementById('status').innerHTML ='Logout realizado. \n Por favor haga Log in de nuevo para disfrutar de las herramienta de la web. '
+            document.getElementById('btn-log-out').style.display = 'none';
+            window.location.href = "/";
+         });
       }
    </script>
-   <script Language="JavaScript">
-   
-      <!-- Helpers for JSI page...-->
-      // Navigation - Start
-      function goback(){
-      alert("Good Bye!");
-      history.go(-1);
-      }
-      function gettheDate() {
-      Todays = new Date();
-      TheDate = "" + (Todays.getMonth()+ 1) +" / "+ Todays.getDate() + " / " + (Todays.getYear() + 1900) 
-      document.clock.thedate.value = TheDate;
-      }
-      // Navigation - Stop
-      // Netscapes Clock - Start
-      // this code was taken from Netscapes JavaScript documentation at
-      // www.netscape.com on Jan.25.96
-      var timerID = null;
-      var timerRunning = false;
-      function stopclock (){
-      if(timerRunning)
-      clearTimeout(timerID);
-      timerRunning = false;
-      }
-      function startclock () {
-      // Make sure the clock is stopped
-      stopclock();
-      gettheDate()
-      showtime();
-      }
-      function showtime () {
-      var now = new Date();
-      var hours = now.getHours();
-      var minutes = now.getMinutes();
-      var seconds = now.getSeconds()
-      var timeValue = "" + ((hours >12) ? hours -12 :hours)
-      timeValue += ((minutes < 10) ? ":0" : ":") + minutes
-      timeValue += ((seconds < 10) ? ":0" : ":") + seconds
-      timeValue += (hours >= 12) ? " P.M." : " A.M."
-      document.clock.face.value = timeValue;
-      // you could replace the above with this
-      // and have a clock on the status bar:
-      // window.status = timeValue;
-      timerID = setTimeout("showtime()",1000);
-      timerRunning = true;
-      }
-      // Netscapes Clock - Stop
-      // end Helpers -->
-   
-   </script>
-
 </head>
- 
-<body style=background-color:#FFFFCC;>
+
+
+<body onload="checkLoginState();" style=background-color:#FFFFCC; margin-left:2cm>
  
     <div class="container">
         <div class="header">
             <nav>
                 <ul class="nav nav-pills pull-right">
-                    <li role="presentation" class="active"><a href="/">Home</a>
+                    <li role="presentation" class="active"><a href="/">  Home  </a>
                     </li>
-                    <li role="presentation"><a href="#">  Sign In  </a>
-                    </li>
-                    <li role="presentation"><a href="showSignUp">  Sign Up  </a>
+                    <li id="btn-log-out">
+                      <a onclick="mysignout();" href="#">Log out</a>
                     </li>
                 </ul>
             </nav>
-            <h3 class="text-muted">Resultados</h3>
         </div>
- 
-        <p>Umbral Superior: {{umbsup}}</p>
-        <p>Umbral Inferior: {{umbinf}}</p>
-        <h2 class="text-muted">Resultados Superiores:</h2> 
-            <!--<li>{{listsup}}</li>-->
-            {{listsup}}
-        <h2 class="text-muted">Resultados Inferiores:</h2>
-            <input type="button" value="Genera una tabla" onclick="genera_tabla({{listinf}})">
-            <!--<li >{{listinf}}</li>-->
-            {{listinf}}
-        <h2 class="text-muted">Resultados Superiores (Beebotte):</h2> 
-            <!--<li>{{listsup}}</li>-->
-            {{lbbtsup}}
-        <h2 class="text-muted">Resultados Inferiores (Beebotte):</h2>
-            <!--<table width="100%" border="1">
-               <?php $arrays={{listinf}}; ?>
-               <?php foreach($arrays as $array):?>
-                     <li><?php echo $arrays ?></li>
-               <?php endforeach;?>
-            </table>-->
-            <!--<li >{{listinf}}</li>-->
-            {{lbbtinf}}
-        <table border>
-         <tr>
-            <td><form name="clock" onSubmit="0"></td>
-         </tr>
-         <tr>
-            <td colspan=2>Hoy es: <input type="text" name="thedate" size=12 value=""></td>
-            <td colspan=2>La hora es: <input type="text" name="face" size=12 value=""></td></form>
-         </tr>
-         </table>
-        <footer class="footer">
+        
+        <div class="principal">
+            <h3 class="text-muted">Random Numbers App</h3>
+            </p>
+        </div>
+        <div id="status">
+        </div>
+        <center>
+           <div style="text-align:center;padding:1em 0;"> 
+               <h4>
+                  <a style="text-decoration:none;" href="https://www.zeitverschiebung.net/es/city/3117735"><span style="color:gray;">Hora actual en</span><br />Madrid, Spain</a>
+               </h4> 
+               <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=es&size=small&timezone=Europe%2FMadrid" width="100%" height="90" frameborder="0" seamless></iframe>
+           </div>
+           <h3 class="text-muted">Resultados</h3>
+           <p><b>  Umbral Superior:</b> {{umbsup}}</p>
+           <p><b>  Umbral Inferior:</b> {{umbinf}}</p>
+           <p> ----------------------------- </p>
+           <script type="text/javascript" margin-left:20px;>
+               genera_tabla({{listsup}},{{listinf}},{{lbbtsup}},{{lbbtinf}});
+           </script>
+           <footer class="footer">
             <p>&copy; Natalia de la Hoz Carracedo - Computacion en red - UAH 2017</p>
-        </footer>
- 
+           </footer>
+        </center>
     </div>
 </body>
+
  
 </html>
